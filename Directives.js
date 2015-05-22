889065ed375e83a507a1cda8be91a75274bb205b
Milen Mavrov <m.mavr0v@yahoo.com>
2015-05-22 16:08:12 +0300

    Two way data binding example.

--- Directives.js
+++ Directives.js
@@ -1,11 +1,16 @@
-angular.module('myApp', [])
-.directive('myDirective', function(){
-  return {
-    restrict: 'A',
-    scope: {
-      myUrl: '@',
-      myLinkText: '@'
-    },
-    template: '<a href="{{myUrl}}">{{myLinkText}}</a>'
-  };
-});
+angular
+    .module('myApp', [])
+    .directive('myDirective', function () {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {
+                myUrl: "=",
+                myLinkText: "@"
+            },
+            template: '<div> ' +
+            '<input type="text" ng-model="myUrl"/> ' +
+            '<a href="{{myUrl}}">{{ myLinkText }}</a> ' +
+            '</div>'
+        };
+    });
