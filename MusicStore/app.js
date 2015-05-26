d96291867eb82e597c36b098fac3b18c67b2b284
m.mavrov <m.mavrov@fio.de>
2015-05-26 18:37:38 +0300

    Add pictures and tabs

--- MusicStore/app.js
+++ MusicStore/app.js
@@ -7,18 +7,36 @@
         vm.products = instruments;
     });
 
+    app.controller('TabController', function(){
+        var vm = this;
+
+        vm.tab = 1;
+
+        vm.setTab = function(tab){
+            this.tab = tab;
+        };
+        vm.isSelected = function(tab){
+            return this.tab === tab;
+        };
+    });
+
     var instruments = [
-        {
-            name: 'Drums',
-            price: 1200.95,
-            canPurchase: true,
-            soldOut: false
-        },
         {
             name: 'Guitar',
             price: 2200.99,
+            canPurchase: true,
+            soldOut: false,
+            description: 'Fender',
+            styles: ['metal', 'jazz'],
+            image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSxh0QPteFAVRKNDCoAohIQMDuQL9p9yW4erQo3T0jOBEHBTyXM3Q'
+        },
+        {
+            name: 'Drums',
+            price: 1200.95,
             canPurchase: false,
-            soldOut: false
+            soldOut: false,
+            description: 'TAMA',
+            styles: ['country', 'metal'],
+            image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcStZWObF8wU0BFg6WPiQ5mc1R6d1BdNVirC5TC_KoBh1KJHbdfN'
         }];
-
 })();
