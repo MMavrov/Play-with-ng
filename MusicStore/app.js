47432fb3a303d781bdaad2501df09d69e5fd586d
m.mavrov <m.mavrov@fio.de>
2015-05-27 09:41:48 +0300

    Reviews added

--- MusicStore/app.js
+++ MusicStore/app.js
@@ -28,15 +28,36 @@
             soldOut: false,
             description: 'Fender',
             styles: ['metal', 'jazz'],
-            image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSxh0QPteFAVRKNDCoAohIQMDuQL9p9yW4erQo3T0jOBEHBTyXM3Q'
-        },
-        {
+            image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSxh0QPteFAVRKNDCoAohIQMDuQL9p9yW4erQo3T0jOBEHBTyXM3Q',
+            reviews: [{
+                stars: 5,
+                body: "I love this guitar!",
+                author: "joe@example.org",
+                createdOn: 1397490980837
+            }, {
+                stars: 1,
+                body: "This guitar sucks.",
+                author: "tim@example.org",
+                createdOn: 1397490980837
+            }]
+        }, {
             name: 'Drums',
             price: 1200.95,
             canPurchase: true,
             soldOut: false,
             description: 'TAMA',
             styles: ['country', 'metal'],
-            image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcStZWObF8wU0BFg6WPiQ5mc1R6d1BdNVirC5TC_KoBh1KJHbdfN'
+            image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcStZWObF8wU0BFg6WPiQ5mc1R6d1BdNVirC5TC_KoBh1KJHbdfN',
+            reviews: [{
+                stars: 3,
+                body: "I think those drums were just OK, could honestly use more shine, IMO.",
+                author: "JimmyDean@example.org",
+                createdOn: 1397490980837
+            }, {
+                stars: 4,
+                body: "Those drums are for me!",
+                author: "gemsRock@example.org",
+                createdOn: 1397490980837
+            }]
         }];
 })();
