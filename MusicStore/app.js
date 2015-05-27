05e89e328343a04924c074f306601f8fa756d72a
m.mavrov <m.mavrov@fio.de>
2015-05-27 10:40:25 +0300

    Fix review submission and add controller

--- MusicStore/app.js
+++ MusicStore/app.js
@@ -20,6 +20,16 @@
         };
     });
 
+    app.controller('ReviewController', function() {
+        this.review = {};
+
+        this.addReview = function(product) {
+            product.reviews.push(this.review);
+
+            this.review = {};
+        };
+    });
+
     var instruments = [
         {
             name: 'Guitar',
