fac795ec5a10875b5915f036732eeb0c197efc8c
m.mavrov <m.mavrov@fio.de>
2015-05-27 12:46:00 +0300

    Refactoring with directives

--- MusicStore/app.js
+++ MusicStore/app.js
@@ -20,17 +20,59 @@
         };
     });
 
-    app.controller('ReviewController', function() {
+    app.directive("productTabs", function(){
+        return {
+            restrict: 'E',
+            templateUrl: "product-tabs.html",
+            controller: function() {
+                var vm = this;
+                vm.tab = 1;
+
+                vm.isSelected = function(checkTab) {
+                    return vm.tab === checkTab;
+                };
+
+                vm.setTab = function(setTab) {
+                    vm.tab = setTab;
+                };
+            },
+            controllerAs: 'tabController'
+
+        };
+    });
+
+    app.controller("ReviewController", function(){
         var vm = this;
 
         vm.review = {};
 
-        vm.addReview = function(product) {
+        vm.addReview = function(product){
             vm.review.createdOn = Date.now();
 
             product.reviews.push(vm.review);
-
             vm.review = {};
+        };
+
+    });
+
+    app.directive('productReviews', function(){
+        return {
+            restrict: 'E',
+            templateUrl: "product-reviews.html"
+        };
+    });
+
+    app.directive("productSpecs", function() {
+        return {
+            restrict:"A",
+            templateUrl: "product-specs.html"
+        };
+    });
+
+    app.directive("productDescription", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-description.html"
         };
     });
 
@@ -52,7 +94,7 @@
                 stars: 1,
                 body: "This guitar sucks.",
                 author: "tim@example.org",
-                createdOn: 1397490980837
+                createdOn: 1397794918378
             }]
         }, {
             name: 'Drums',
